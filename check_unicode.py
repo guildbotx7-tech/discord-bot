156d9af01a363dc8a108b9eac0e1a4c654fabec4
Pikachu-exe <guildbotx7@gmail.com>
2026-04-11 16:29:22 +0530

    Cleanup unwanted files: remove cache dirs, duplicates, and unused scripts

--- check_unicode.py
+++
@@ -1,6 +0,0 @@
-with open('commands/reconcile_bot.py', 'r') as f:
-    content = f.read()
-non_ascii = [char for char in content if ord(char) > 127]
-print('Non-ASCII characters found:', len(non_ascii))
-if non_ascii:
-    print('Characters:', set(non_ascii))
